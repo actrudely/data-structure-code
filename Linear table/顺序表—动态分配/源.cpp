4c221f1a386e123fd02067be633b29d28708b7d7
actrudely <1484009238@qq.com>
2022-08-22 17:26:20 +0800

    线性表

--- Linear table/顺序表—动态分配/源.cpp
+++ Linear table/顺序表—动态分配/源.cpp
@@ -10,6 +10,8 @@ typedef struct
 	ElemType* data;
 	int length, MAXSIZE;
 }SeqList;
+//定义data指针来表示顺序表，以及length表示表长
+//MAXSIZE必不可少—动态分配增加时需要将最大长度增加，InitSize固定数值无法改变
 
 Status InitList(SeqList& L)
 {
@@ -18,19 +20,20 @@ Status InitList(SeqList& L)
 	L.MAXSIZE = InitSize;
 	return true;
 }
+//初始化动态分配，表长置为0，使MAXSIZE为最大长度
 
 Status IncreaseSize(SeqList& L,int i)
 {
-	ElemType* p;
-	p = L.data;
+	ElemType* p = L.data;
 	L.data = new ElemType[L.MAXSIZE + i];
 	for (int i = 0; i < L.length; i++)
 		L.data[i] = p[i];
-	//InitSize = InitSize + i;
+	//InitSize = InitSize + i;InitSize固定无法更改
 	L.MAXSIZE = L.MAXSIZE + i;
 	delete p;
 	return true;
 }
+//增加动态数组长度，需要数次移动已有元素，并更改最大长度
 
 int main()
 {
